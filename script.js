04d72c1bdf5d018e841edc11cbb61a26f86f2c3c
sablefluff <sablefluff3@gmail.com>
2026-06-24 19:42:33 -0400

    fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ if (sidebarElement) {
     sidebarElement.innerHTML = "Failed to load :("
 
     var xhr= new XMLHttpRequest(); // https://stackoverflow.com/questions/3535055/load-html-file-contents-to-div-without-the-use-of-iframes
-    xhr.open("GET", "sidebar.html", true);
+    xhr.open("GET", "/sidebar.html", true);
     xhr.onreadystatechange= function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return; // or whatever error handling you want
